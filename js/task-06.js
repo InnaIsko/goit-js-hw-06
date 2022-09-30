bafeeb918a0d21f9b6cb65889997298d5e6ac255
Inna <isko.inna.94@gmail.com>
2022-09-30 12:53:54 +0300

    зміни у 6й задачі

--- js/task-06.js
+++ js/task-06.js
@@ -4,10 +4,11 @@ const placeholder = input.getAttribute("placeholder");
 
 const inputClick = (event) => {
   console.log(event.currentTarget.value.length);
-  if (dataLength == event.currentTarget.value.length) {
-    input.classList.add("valid");
-  } else {
+  if (dataLength != event.currentTarget.value.length) {
     input.classList.add("invalid");
+  } else if (dataLength == event.currentTarget.value.length) {
+    input.classList.remove("invalid");
+    input.classList.add("valid");
   }
 };
 console.log(input.value);
