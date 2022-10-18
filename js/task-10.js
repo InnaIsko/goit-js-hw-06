406ed25e14d608b6685c9b3a25bfde056704f5be
Inna <isko.inna.94@gmail.com>
2022-10-18 20:24:21 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,44 +1,31 @@
-// // Напиши скрипт создания и очистки коллекции элементов.
-// // Пользователь вводит количество элементов в input и нажимает
-// // кнопку Создать, после чего рендерится коллекция.При нажатии
-// // на кнопку Очистить, коллекция элементов очищается.
-// // Создай функцию createBoxes(amount), которая
-// // принимает один параметр - число.Функция создает столько
-// // < div >, сколько указано в amount и добавляет их в div#boxes.
+const divControlsRef = document.querySelector("#controls");
+const divBoxRef = document.querySelector("#boxes");
+const inputRef = divControlsRef.firstElementChild;
+const btnCreateRef = inputRef.nextElementSibling;
+const btnDestroy = btnCreateRef.nextElementSibling;
 
-// // Размеры самого первого <div> - 30px на 30px.
-// // // Каждый элемент после первого, должен быть шире и выше
-// // предыдущего на 10px.
-// // Все элементы должены иметь случайный цвет фона в формате HEX.
-// // Используй готовую функцию getRandomHexColor для получения цвета.
-// const divBtn = document.querySelector("#controls");
-// const divBox = document.querySelector("#boxes");
-// const input = divBtn.firstElementChild;
-// const btnCreate = input.nextElementSibling;
-// const btnDestroy = btnCreate.nextElementSibling;
+function btnHandler() {
+  const inputValue = Number(inputRef.value);
+  createBoxes(inputValue);
+}
 
-// console.log(btnDestroy);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-// const inputRange = (event) => {
-//   const amount = event.currentTarget.value;
-//   return amount;
-// };
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+function createBoxes(amount) {
+  let basicSize = 30;
+  for (let index = 1; index <= amount; index += 1) {
+    basicSize += 10 * index;
+    const box = `<div style = "width:${basicSize}px; height:${basicSize}px; background-color:${getRandomHexColor()};"></div>`;
+    divBoxRef.insertAdjacentHTML("beforeend", box);
+  }
+}
 
-// function createBoxes(amount) {
-//   let basicSize = 30;
-//   const color = getRandomHexColor();
-//   for (let index = 0; index <= amount; index += 1) {
-//     let size = basicSize + i + 10;
-//     let div = document.createElement("div");
-//     div.style.width = `${size}px`;
-//     div.style.height = `${size} px`;
-//     div.style.backgroundColor = color;
-//     divBox.append(...div);
-//   }
-// }
+function destroyBoxes() {
+  inputRef.value = "";
+  divBoxRef.innerHTML = "";
+}
 
-// btnCreate.addEventListener("click", createBoxes);
-// input.addEventListener("change", inputRange);
+btnCreateRef.addEventListener("click", btnHandler);
+btnDestroy.addEventListener("click", destroyBoxes);
